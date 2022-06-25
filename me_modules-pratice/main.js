a1c4b5bcb06c8c810bc1aa2fed7f4d588de0b682
Keron Williams <ikeronw@gmail.com>
2022-06-25 00:44:47 -0400

    made changes

--- me_modules-pratice/main.js
+++ me_modules-pratice/main.js
@@ -1,3 +1,6 @@
+// console.log(`---- EXPORTING AND IMPORTING IN ES6 MODULES ---`);
+// https://www.udemy.com/course/the-complete-javascript-course/learn/lecture/22649463#questions
+
 import { hi } from './hello.js';
 
 hi();
